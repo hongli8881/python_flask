21174b591e294373cdaaf53875485edc158bc86a
hongli8881 <hong3.li@citi.com>
2019-01-20 09:59:50 -0500

    Add files via upload

---
+++ viv_app/app.py
@@ -0,0 +1,4 @@
+
+from flask import Flask
+vivi_app = Flask(__name__)
+
---
+++ viv_app/vivi_app.py
@@ -0,0 +1,13 @@
+from home.home import home_page
+from flask import Flask
+from login.login import login_page
+from admin.admin import admin_page
+from common.imports import os
+from app import vivi_app
+   
+vivi_app.register_blueprint(home_page)
+vivi_app.register_blueprint(login_page)
+vivi_app.register_blueprint(admin_page)
+vivi_app.secret_key = os.urandom(24)
+
+vivi_app.run(host='127.0.0.1', port=8080)
